a0de2f19643b06195f345f091881c96093e2fb63
Eze Opara <122412102+victorezeopara@users.noreply.github.com>
2024-07-16 12:03:01 -0700

    Update bookSession.js

--- controllers/bookSession.js
+++ controllers/bookSession.js
@@ -36,8 +36,8 @@ router.use("/", async function (req, res, next) {
     // console.log("Document written with ID: ", docRef.id);
 
     const mailOptions = {
-      from: "emmanuel.sheshi101@gmail.com",
-      to: "emmanuel.sheshi@gmail.com",
+      from: "oparavictoreze@gmail.com",
+      to: "oparavictoreze@gmail.com",
       subject: "New Booking Notification",
       html: `
     <!DOCTYPE html>
